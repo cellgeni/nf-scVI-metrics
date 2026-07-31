ef3bbf0d81ab0b3b6a9f138647005b9ad09e7752
Vijaya Baskar <34027254+vjbaskar@users.noreply.github.com>
2026-07-31 05:52:30 +0100

    Refactor plot_umap.py to load parameters from YAML file
    - Replaced run_path with YAML file handling for input parameters.
    - Updated UMAP plotting logic to use metrics extracted from the YAML configuration.
    - Enhanced code readability and maintainability by adopting a consistent parameter loading approach.

--- bin/plot_umap.py
+++ bin/plot_umap.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-from runpy import run_path
+import yaml
 
 parser = argparse.ArgumentParser(description='Plot UMAP results.')
 parser.add_argument('--adata', type=str, help='Path to the AnnData file.')
@@ -16,7 +16,9 @@ parser.add_argument('--umaps', type=str, help='Space-separated list of UMAP nump
 args = parser.parse_args()
 
 adata = ad.read_h5ad(args.adata, backed='r')
-params = run_path(args.input_file)
+with open(args.input_file, "r", encoding="utf-8") as handle:
+    params = yaml.safe_load(handle)
+scib_metrics = params["scib"]["metrics"]
 
 pp = PdfPages("umap.pdf")
 sc.set_figure_params(figsize=(8, 8))
@@ -24,7 +26,7 @@ for i in sorted(args.umaps.split()):
     umap = np.load(i)
     adata.obsm['X_umap'] = umap
     plt.clf()
-    fig = sc.pl.umap(adata, color=[params['scib_input']['batch_key'], params['scib_input']['label_key']], 
+    fig = sc.pl.umap(adata, color=[scib_metrics['batch_key'], scib_metrics['label_key']], 
                      return_fig=True, show=False)
     fig.suptitle(i.split('.')[0])
     pp.savefig(fig, bbox_inches='tight')
